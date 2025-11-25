ffabe011139a6a70215d04b375db1e5c00055aae
Diana <dianamilcovich@gmail.com>
2025-11-26 04:06:08 +1000

    Изменение динамического массива на вектор

--- LAB_2.cpp
+++ LAB_2.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <chrono>
 #include <cstdlib>
+#include<vector>
 #include <ctime>
 #include <iomanip>
 using namespace std;
@@ -25,31 +26,31 @@ struct key {
     int num_str;
 };
 
-void print(key* data, int N){
+void print(vector <key> data, int N){
     for (int i = 0; i < N; i++) {
         cout << i + 1 << ". " << data[i].data.day << "." << data[i].data.month << "." << data[i].data.year << " "
              << data[i].fio.F << " " << data[i].fio.I << " " << data[i].fio.O << "\n";
     }
 }
 
-void sort_shaker(key* data, int n) {
+void sort_shaker(vector <key>& data, int n) {
     int left = 0;
     int right = n - 1;
     while(left < right){
         for(int i = left; i < right; i++) {
             int flag = 0;
 
-            // Дата по ВОЗРАСТАНИЮ (как в sort_data)
+            // Дата по ВОЗРАСТАНИЮ
             if (data[i].data.year > data[i+1].data.year
                 || (data[i].data.year == data[i+1].data.year && data[i].data.month > data[i+1].data.month)
                 || (data[i].data.year == data[i+1].data.year && data[i].data.month == data[i+1].data.month && data[i].data.day > data[i+1].data.day)) {
                 flag = 1;
             }
-            // Если даты равны, ФИО по УБЫВАНИЮ (как в sort_name)
+            // Если даты равны, ФИО по УБЫВАНИЮ
             else if (data[i].data.year == data[i+1].data.year
                      && data[i].data.month == data[i+1].data.month
                      && data[i].data.day == data[i+1].data.day) {
-                if (data[i].fio.F < data[i+1].fio.F ||  // ЗНАК < для убывания
+                if (data[i].fio.F < data[i+1].fio.F ||
                    (data[i].fio.F == data[i+1].fio.F && data[i].fio.I < data[i+1].fio.I) ||
                    (data[i].fio.F == data[i+1].fio.F && data[i].fio.I == data[i+1].fio.I && data[i].fio.O < data[i+1].fio.O)) {
                     flag = 1;
@@ -67,13 +68,13 @@ void sort_shaker(key* data, int n) {
         for(int i = right; i > left; i--){
             int swap_flag = 0;
 
-            // Дата по ВОЗРАСТАНИЮ (как в sort_data)
+            // Дата по ВОЗРАСТАНИЮ
             if (data[i-1].data.year > data[i].data.year
                 || (data[i-1].data.year == data[i].data.year && data[i-1].data.month > data[i].data.month)
                 || (data[i-1].data.year == data[i].data.year && data[i-1].data.month == data[i].data.month && data[i-1].data.day > data[i].data.day)) {
                 swap_flag = 1;
             }
-            // Если даты равны, ФИО по УБЫВАНИЮ (как в sort_name)
+            // Если даты равны, ФИО по УБЫВАНИЮ
             else if (data[i-1].data.year == data[i].data.year
                      && data[i-1].data.month == data[i].data.month
                      && data[i-1].data.day == data[i].data.day) {
@@ -93,13 +94,13 @@ void sort_shaker(key* data, int n) {
         left++;
     }
 }
-void sort_Shella(key* data, int n) {
+void sort_Shella(vector <key>& data, int n) {
     int gap = n / 2;
     while (gap > 0) {
         for (int i = gap; i < n; i++) {
             int j = i;
             while (j >= gap) {
-                // Сравнение по дате
+
                 if (data[j].data.year < data[j - gap].data.year ||
                     (data[j].data.year == data[j - gap].data.year &&
                      data[j].data.month < data[j - gap].data.month) ||
@@ -107,7 +108,7 @@ void sort_Shella(key* data, int n) {
                      data[j].data.month == data[j - gap].data.month &&
                      data[j].data.day < data[j - gap].data.day)) {
 
-                    // Замена через временную переменную
+
                     key temp = data[j];
                     data[j] = data[j - gap];
                     data[j - gap] = temp;
@@ -140,12 +141,11 @@ void sort_Shella(key* data, int n) {
 int main(){
     system("chcp 65001");
     int N = 6;
-    key* mas =new key[N];
+    vector <key> mas(N);
     ifstream file("data.txt");
 
     if (!file.is_open()) {
         cout << "Не удалось открыть файл!" << endl;
-        delete[] mas;
         return 1;
     }
     string line;
@@ -178,42 +178,19 @@ int main(){
     }
     out_file_shaker << "Время, затраченное на сортировку: " << duration.count() << " мс" << endl;
 
-    start = high_resolution_clock::now();
-    sort_shaker(mas, N);
-    end = high_resolution_clock::now();
-    duration = duration_cast<milliseconds>(end - start);
-    out_file_shaker << "CОРТИРОВКА ПО ФИО\n";
-    for(int i = 0; i < N; i++){
-        out_file_shaker << mas[i].data.day << "." << mas[i].data.month << "." << mas[i].data.year << " "
-        << mas[i].fio.F << " " << mas[i].fio.I << " " << mas[i].fio.O << " " << mas[i].num_str << endl;
-    }
-    out_file_shaker << "Время, затраченное на сортировку: " << duration.count() << " мс";
-    out_file_shaker.close();
+
 
     ofstream out_file_shell("out_data_shell");
     start = high_resolution_clock::now();
     sort_Shella(mas, N);
     end = high_resolution_clock::now();
     duration = duration_cast<milliseconds>(end - start);
-    out_file_shell << "CОРТИРОВКА ПО ДАТЕ\n";
+    out_file_shell << "CОРТИРОВКА\n";
     for(int i = 0; i < N; i++){
         out_file_shell << mas[i].data.day << "." << mas[i].data.month << "." << mas[i].data.year << " "
         << mas[i].fio.F << " " << mas[i].fio.I << " " << mas[i].fio.O << " " << mas[i].num_str << endl;
     }
     out_file_shell << "Время, затраченное на сортировку: " << duration.count() << " мс" << endl;
 
-    start = high_resolution_clock::now();
-    sort_Shella(mas, N);
-    end = high_resolution_clock::now();
-    duration = duration_cast<milliseconds>(end - start);
-    out_file_shell << "CОРТИРОВКА ПО ФИО\n";
-    for(int i = 0; i < N; i++){
-        out_file_shell << mas[i].data.day << "." << mas[i].data.month << "." << mas[i].data.year << " "
-        << mas[i].fio.F << " " << mas[i].fio.I << " " << mas[i].fio.O << " " << mas[i].num_str << endl;
-    }
-    out_file_shell << "Время, затраченное на сортировку: " << duration.count() << " мс";
-    out_file_shell.close();
-
-    delete[] mas;
     file.close();
 }
