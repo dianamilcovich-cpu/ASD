26995f933db7ecaff788c398866d3bea41e5b958
theApsil <goncharuk.dm@students.dvfu.ru>
2025-12-21 14:33:17 +1000

    FIX: Incorrect file reading

--- LAB_3/src/LAB_3.cpp
+++ LAB_3/src/LAB_3.cpp
@@ -8,14 +8,20 @@
 #include"Structs/structs.h"
 using namespace std;
 int main() {
-    system("chcp 65001");
-    int n = 1000000;
+    setlocale(LC_ALL, "rus");
+
+    int n = 123456;
     vector<Record> records(n);
-    int key = 287191;
-    if (!readFile("../LAB_3/Static/input_100000.txt", n, records)) {
+    int key = 999;
+    if (!readFile("../LAB_3/Static/input_10.txt", n, records)) {
         writeFile("../Static/output.txt",-1,0);
         return 0;
     }
+
+    for (int i{}; i < static_cast<int>(records.size()); ++i){
+        std::cout << records[i].fio.f << std::endl;
+    }
+
     if (records.size() != n) {
         cout << "Прочитано " << records.size() << " записей вместо " << n << endl;
     }
@@ -28,6 +34,6 @@ int main() {
     int res = -1;
     if (pos != -1)
         res = records[pos].StrokeNumber;
-    writeFile("../LAB_3/Static/output.txt",res,steps);
+    writeFile("../LAB_3/Static/output2.txt",res,steps);
     return 0;
 }
